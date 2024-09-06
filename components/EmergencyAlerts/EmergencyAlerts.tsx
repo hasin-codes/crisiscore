ff89735ddb03044940eacfc1d4dd918d4b887dd6
Hasin Raiyan <fullcopyrightsounds@gmail.com>
2024-09-07 02:52:37 +0600

    upppp

--- components/EmergencyAlerts/EmergencyAlerts.tsx
+++ components/EmergencyAlerts/EmergencyAlerts.tsx
@@ -4,7 +4,7 @@ import { AlertTriangle } from "lucide-react"
 
 const EmergencyAlerts = () => {
   return (
-    <Card className="border-red-700 shadow-[0_0_10px_rgba(255,255,255,0.1)] bg-white bg-opacity-10 backdrop-blur-[7px]">
+    <Card className="border-red-700 shadow-[0_0_10px_rgba(255,255,255,0.1)] bg-white bg-opacity-10 backdrop-blur-[7px] h-[300px] ">
       <CardHeader>
         <CardTitle className="flex items-center text-white">
           <AlertTriangle className="w-6 h-6 mr-2" />
@@ -20,6 +20,14 @@ const EmergencyAlerts = () => {
           <li className="flex items-center">
             <span className="w-2 h-2 bg-yellow-500 rounded-full mr-2"></span>
             Evacuation order for Sector 7
+          </li>
+          <li className="flex items-center">
+            <span className="w-2 h-2 bg-orange-500 rounded-full mr-2"></span>
+            High wind warning for coastal areas
+          </li>
+          <li className="flex items-center">
+            <span className="w-2 h-2 bg-blue-500 rounded-full mr-2"></span>
+            Flash flood watch in effect
           </li>
         </ul>
       </CardContent>
--- components/Planner.tsx
+++ components/Planner.tsx
@@ -7,14 +7,15 @@ import PreparationChecklist from './PreparationChecklist/PreparationChecklist'
 import EmergencyActions from './EmergencyActions/EmergencyActions'
 import AidTracker from './AidTracker/AidTracker'
 import PlannerComponent from './planner/Planner'
+import CurrentAlerts from './CurrentAlerts/CurrentAlerts'
 
 export default function Planner() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6 px-4">
       <PlannerComponent />
-      <EmergencyAlerts />
       <EmergencyAIAssistant />
-      <PreparationChecklist />
+      <EmergencyAlerts />
+      <CurrentAlerts/>
       <EmergencyActions />
       <AidTracker />
     </div>
