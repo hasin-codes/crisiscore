d8af937d6c02ebb4c6ca3bb1b8d0ba4b597635a5
Hasin Raiyan <fullcopyrightsounds@gmail.com>
2024-09-14 07:48:38 +0600

    bug fix

--- components/Emergency.tsx
+++ components/Emergency.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import CycloneWarning from './CycloneWarning/CycloneWarning'
 import ShelterMap from './Shelter/Shelter'
 import EmergencyAlerts from './EmergencyAlerts/EmergencyAlerts'
@@ -10,16 +10,23 @@ import EmergencyActions from './EmergencyActions/EmergencyActions'
 import AidTracker from './AidTracker/AidTracker'
 
 export function Emergency() {
+  const [aiPlanTasks, setAiPlanTasks] = useState([]);
+
+  useEffect(() => {
+    // Fetch or compute aiPlanTasks here
+    // For now, we'll use an empty array as a placeholder
+    setAiPlanTasks([]);
+  }, []);
+
   return (
-      
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6 px-4">
-        <ShelterMap/>
-        <EmergencyAlerts />
-        <EmergencyAIAssistant />
-        <PreparationChecklist />
-        <EmergencyActions />
-        <AidTracker />
-      </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6 px-4">
+      <ShelterMap/>
+      <EmergencyAlerts />
+      <EmergencyAIAssistant />
+      <PreparationChecklist aiPlanTasks={aiPlanTasks} />
+      <EmergencyActions />
+      <AidTracker />
+    </div>
   )
 }
 
